5b15d9c5ff07e5a4eee17ae42a5e2152aeebf045
Sajjad Manal <sajjadmanal24@gmail.com>
2016-07-21 01:01:45 +0530

    Detail Activity

--- java/com/example/android/movies/DetailActivity.java
+++ java/com/example/android/movies/DetailActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+
 public class DetailActivity extends AppCompatActivity {
 
     @Override
