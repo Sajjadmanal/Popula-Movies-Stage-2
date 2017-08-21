c1a0299979bb7a08747217e3953ad187a754e06f
Sajjad Manal <sajjadmanal24@gmail.com>
2017-08-22 00:11:52 +0530

    Update ApplicationTest.java

--- androidTest/java/com/example/android/movies/ApplicationTest.java
+++ androidTest/java/com/example/android/movies/ApplicationTest.java
@@ -1,10 +1,8 @@
 /* ApplicationTest by: Sajjad Manal
 */
 package com.example.android.movies;
-
 import android.app.Application;
 import android.test.ApplicationTestCase;
-
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
