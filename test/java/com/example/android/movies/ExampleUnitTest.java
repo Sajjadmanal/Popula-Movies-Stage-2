490f7360732d7daf44efa9b7c0af2a622dccab77
Sajjad Manal <sajjadmanal24@gmail.com>
2017-08-22 00:10:37 +0530

    Update ExampleUnitTest.java

--- test/java/com/example/android/movies/ExampleUnitTest.java
+++ test/java/com/example/android/movies/ExampleUnitTest.java
@@ -1,7 +1,6 @@
 package com.example.android.movies;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
