1477a3dd96b506bdb04fd1e94683f3926259c781
Sajjad Manal <sajjadmanal24@gmail.com>
2018-06-01 20:54:32 +0530

    Update DetailActivity.java

--- java/com/example/android/movies/DetailActivity.java
+++ java/com/example/android/movies/DetailActivity.java
@@ -20,11 +20,13 @@ public class DetailActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.detail_container, new DetailFragment())
                 .commit();
+       
         //Changing color of back button
         final Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.ic_keyboard_arrow_left_black_24dp);
         upArrow.setColorFilter(ContextCompat.getColor(this, R.color.backButton), PorterDuff.Mode.SRC_ATOP);
         if(getSupportActionBar()!=null)
         getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        
         //Disabling auto title
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
